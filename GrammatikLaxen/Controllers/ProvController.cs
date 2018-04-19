51331c42d12575c4b372c1e158b2d8ad369a2728
nica16oo <nica1600@student.miun.se>
2018-04-19 19:04:43 +0200

    Skapat controller och sida för autogenererade proven.

---
+++ GrammatikLaxen/Controllers/ProvController.cs
@@ -0,0 +1,20 @@
+﻿using GrammatikLaxen.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrammatikLaxen.Controllers
+{
+    public class ProvController : Controller
+    {
+        DbOperations db = new DbOperations();
+
+        // GET: Prov
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
