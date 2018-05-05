aa8ee67a7d13efd32149b194ea3e0cef365f27e6
nica16oo <nica1600@student.miun.se>
2018-05-05 19:42:00 +0200

    Allt utom ordklass-del (fakta) och spelet är klart, inklusive design. Redo för tester.

---
+++ GrammatikLaxen/Controllers/GrammatikController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrammatikLaxen.Controllers
+{
+    public class GrammatikController : Controller
+    {
+        // GET: Grammatik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
