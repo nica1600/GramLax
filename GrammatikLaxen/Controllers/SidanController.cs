f5ec1fb4a6a5e1770d9eb73447449c422cb3040b
nica16oo <nica1600@student.miun.se>
2018-05-29 10:41:28 +0200

    Flyttat Contact och About till egen controller för att få bättre/snyggare sökväg

---
+++ GrammatikLaxen/Controllers/SidanController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrammatikLaxen.Controllers
+{
+    public class SidanController : Controller
+    {
+        // GET: Sidan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Om()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            return View();
+        }
+    }
+}
